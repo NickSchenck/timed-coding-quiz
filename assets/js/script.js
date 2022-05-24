6907c5dcc274c79799ab388d552f7cba3fbd5bbb
NickSchenck <yoyo4games@yahoo.com>
2022-05-24 15:26:26 -0500

    creating next page

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var beginEl = document.querySelector("#btn-begin");
+
+
+beginEl.addEventListener("click", function(){
+    var opened = window.open("");
+opened.document.write("<html><head><title>MyTitle</title></head><body><main><h1>Coding Question</h1></main></body></html>");
+    var codingQuestion = document.createElement("h1")
+        codingQuestion.textContent = "test";
+        codingQuestion;
+
+
+}); 
