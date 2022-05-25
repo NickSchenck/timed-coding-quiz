97a56a280ec7d028ce064b63a88a641c8b60ee63
NickSchenck <yoyo4games@yahoo.com>
2022-05-25 14:52:15 -0500

    Page 1 to page 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,60 @@
 var beginEl = document.querySelector("#btn-begin");
+var textEl = document.querySelector("#first-text");
+var nextPage = document.querySelector("#input-given");
+
+var allQuestions = [
+    {
+        question: "What color is your hair?",
+        choices: ["BRN", "BLK", "BLND", "RED"],
+        answer: 3  
+    },
+    {
+        question: "a",
+        choices: ["a", "a", "a", "a"],
+        answer: 3
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: 3
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: 3
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: 3
+    },
+    
+];
+console.dir(allQuestions)
 
 
-beginEl.addEventListener("click", function(){
-    var opened = window.open("");
-opened.document.write("<html><head><title>MyTitle</title></head><body><main><h1>Coding Question</h1></main></body></html>");
-    var codingQuestion = document.createElement("h1")
-        codingQuestion.textContent = "test";
-        codingQuestion;
 
 
-}); 
+var startQuiz = function() {
+    beginEl.remove()
+    renderQuestions();
+};
+var renderQuestions = function() {
+    textEl.textContent = allQuestions[0].question
+    renderChoices();
+};
+var renderChoices = function() {
+    for (let i = 0; i < allQuestions[0].choices.length; i++) {
+        var btn = document.createElement('input');
+        btn.id = "input-given"
+        btn.setAttribute('type' , 'button');
+        btn.setAttribute('value', (allQuestions[0].choices[i]));
+        document.body.appendChild(btn);
+    
+    }
+    
+};
+
+beginEl.addEventListener(
+    "click", startQuiz 
+)
