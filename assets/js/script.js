1b813075b92ec27c99be62033e93f79a4c598c88
NickSchenck <yoyo4games@yahoo.com>
2022-05-26 16:35:12 -0500

    question text now appears in question buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
 var questionContainerEl = document.getElementById("question-container");
 var nextQuestion, currentQuestion
 var questionEl = document.getElementById("question")
 var answerButtonsEl = document.getElementById("answer-buttons")
 var questions = [
     {
-        question: "What is 2+2",
+        question: "What is 2+2?",
         answers: [
             { option: "4", correct: true},
             { option: "7", correct: false},
@@ -14,7 +15,7 @@ var questions = [
         ]
     },
     {
-        question: "What is 2+3",
+        question: "What is 2+3?",
         answers: [
             { option: "5", correct: true},
             { option: "7", correct: false},
@@ -23,7 +24,7 @@ var questions = [
         ]
     },
     {
-        question: "What is 2+1",
+        question: "What is 2+1?",
         answers: [
             { option: "3", correct: true},
             { option: "7", correct: false},
@@ -32,7 +33,7 @@ var questions = [
         ]
     },
     {
-        question: "What is 2+7",
+        question: "What is 2+7?",
         answers: [
             { option: "9", correct: true},
             { option: "7", correct: false},
@@ -41,7 +42,7 @@ var questions = [
         ]
     },
     {
-        question: "What is 2+0",
+        question: "What is 2+0?",
         answers: [
             { option: "2", correct: true},
             { option: "7", correct: false},
@@ -49,7 +50,7 @@ var questions = [
             { option: "31", correct: false}
         ]
     }
-]
+];
 function startGame() {
 startButton.classList.add("hide")
 nextQuestion = questions.sort(() => Math.random() - .5)
@@ -59,13 +60,30 @@ setNextQuestion();
 };
 
 function setNextQuestion() {
+    resetState();
     showQuestion(nextQuestion[currentQuestion])
 };
 function showQuestion(question) {
     questionEl.innerText= question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.option
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        };
+        button.addEventListener("click", selectAnswer)
+        answerButtonsEl.appendChild(button)
+    });
+};
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
 };
 
-function selectAnswer() {
+function selectAnswer(e) {
 
 };
 
