55c1bb1e90f487f32ece2b0e9331c75fae3ec9e3
NickSchenck <yoyo4games@yahoo.com>
2022-05-26 13:10:31 -0500

    start button functional, rendering question+answer buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,68 @@
 var startButton = document.getElementById("start-btn");
 var questionContainerEl = document.getElementById("question-container");
-
+var nextQuestion, currentQuestion
+var questionEl = document.getElementById("question")
+var answerButtonsEl = document.getElementById("answer-buttons")
+var questions = [
+    {
+        question: "What is 2+2",
+        answers: [
+            { option: "4", correct: true},
+            { option: "7", correct: false},
+            { option: "99", correct: false},
+            { option: "31", correct: false}
+        ]
+    },
+    {
+        question: "What is 2+3",
+        answers: [
+            { option: "5", correct: true},
+            { option: "7", correct: false},
+            { option: "99", correct: false},
+            { option: "31", correct: false}
+        ]
+    },
+    {
+        question: "What is 2+1",
+        answers: [
+            { option: "3", correct: true},
+            { option: "7", correct: false},
+            { option: "99", correct: false},
+            { option: "31", correct: false}
+        ]
+    },
+    {
+        question: "What is 2+7",
+        answers: [
+            { option: "9", correct: true},
+            { option: "7", correct: false},
+            { option: "99", correct: false},
+            { option: "31", correct: false}
+        ]
+    },
+    {
+        question: "What is 2+0",
+        answers: [
+            { option: "2", correct: true},
+            { option: "7", correct: false},
+            { option: "99", correct: false},
+            { option: "31", correct: false}
+        ]
+    }
+]
 function startGame() {
-startGame.classList.add("hide")
+startButton.classList.add("hide")
+nextQuestion = questions.sort(() => Math.random() - .5)
+currentQuestion = 0
 questionContainerEl.classList.remove("hide")
 setNextQuestion();
 };
 
 function setNextQuestion() {
-
+    showQuestion(nextQuestion[currentQuestion])
+};
+function showQuestion(question) {
+    questionEl.innerText= question.question
 };
 
 function selectAnswer() {
