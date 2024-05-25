6b272a40a705638d9c4455a024a55cd135be5e36
NickSchenck <103293341+NickSchenck@users.noreply.github.com>
2024-05-25 18:33:36 -0500

    extended and improved notes on each section/function in the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-//variables for targeting specific elements within the DOM, from the timer itself to the disappearing-on-click
-//start button
+//variables for targeting specific elements within the DOM. In order startButton, scoreButton, nextButton,
+//questionContainerEl, questionEl, answerButtonsEl, and scoreBoard all select elements with varrying Ids
+//that roughly equate to the name of their variable counterpart.
 let startButton = document.getElementById("start-btn");
 let scoreButton = document.getElementById("score-btn");
 let nextButton = document.getElementById("next-btn");
@@ -8,8 +9,8 @@ let questionEl = document.getElementById("question");
 let answerButtonsEl = document.getElementById("answer-buttons");
 let scoreBoard = document.getElementById("score-board");
 
-//a variable for storing time and two initialized variables to act as generic containers that are convinient to
-//assign value as the app progresses
+//sec is a variable for storing time. nextQuestion and currentQuestion are two initialized variables to act as
+//generic containers that will be assigned value as the app progresses.
 let sec = 8;
 let nextQuestion, currentQuestion;
 
@@ -19,11 +20,11 @@ let nextQuestion, currentQuestion;
 //which apparently is inefficient and not totally random, but was alright for smaller arrays
 function shuffle(array) {
   let m = array.length, t, i;
-  // While there remain elements to shuffle…
+  // While there remain elements to shuffle
   while (m) {
-    // Pick a remaining element…
+    // Pick a remaining element
     i = Math.floor(Math.random() * m--);
-    // And swap it with the current element.
+    // And swap it with the current element
     t = array[m];
     array[m] = array[i];
     array[i] = t;
@@ -31,8 +32,9 @@ function shuffle(array) {
   return array;
 }
 
-
-//An array containing several objects formatted as questions, to be populated as the quiz is progressed
+//questions is an array containing several objects formatted as questions. Each object is
+//denoted by the curly-brace containing the individual question to the curly-brace just under
+//the answers property array.
 let questions = [
   {
     question: "What is a pseudo-class?",
@@ -81,11 +83,11 @@ let questions = [
   }
 ];
 
-//app is started through an event listener which calls this function. startGame first calls the timer, hides the
-//start button after the user has clicked it, and removes the 'hide' class from questionContainerEl which makes
-//it visible. The container variable nextQuestion is initialized with a value that calls our randomize function
-//on the array of questions, and currentQuestion is given a value of 0 to ensure we start at the beginning of our 
-//now randomized nextQuestion array, when we call the function setNextQuestion.
+//app is started through a click event listener which calls this function. startGame first calls the timer function,
+//uses the startButton variable to add a class of 'hide' to an element, and removes the 'hide' class from
+//questionContainerEl which makes its element visible. The container variable nextQuestion is initialized with a value
+//that calls our shuffle function on the array of questions, and currentQuestion is given a value of 0, which ensures
+//we will start at the beginning of our now randomized nextQuestion array, when we call the function setNextQuestion.
 function startGame() {
   timer();
   startButton.classList.add("hide");
@@ -95,47 +97,67 @@ function startGame() {
   setNextQuestion();
 };
 
-//setNextQuestion first hides the nextButton, after the element has been clicked. We enter a while loop which
-//checks if the provided argument is truthy, and if so prevents the empty buttons from our html file from
-//generating alongside our actual answer buttons. We then call showQuestion with the argument of nextQuestion-
-//our randomized array of questions- at an index of currentQuestion.
+//setNextQuestion first uses the nextButton variable to add a class of 'hide' to its element. Then we enter a while loop
+//which checks if the provided argument is truthy, and if so prevents the empty buttons from our html file from
+//generating. We then call the showQuestion function with the argument of nextQuestion- our randomized array of questions-
+//at an index of currentQuestion(initialized as 0 in the startGame function).
 function setNextQuestion() {
-  nextButton.classList.add("hide"); //hides next button after click
+  nextButton.classList.add("hide");
   while (answerButtonsEl.firstChild) {
-    answerButtonsEl.removeChild(answerButtonsEl.firstChild); //while loop goes through the children of our answer elements
-  }
+    answerButtonsEl.removeChild(answerButtonsEl.firstChild);
+  };
   showQuestion(nextQuestion[currentQuestion]);
 };
 
-//showQuestion first manipulates the text of questionEl by targeting the questions parameter passed to the function
-//and appending the value of question onto it from the object array(questions => question => answers => option).
-//We then target the answers property of the param, putting it through a forEach loop. The forEach loop creates a
-//button for each of the given answer.option values and adds a class of 'btn' to them. Finally, an event listener
-//is attached to button, activating on a click and calling the selectAnswer function.
-function showQuestion(questions) {
-  questionEl.innerText = questions.question; //confusing line, but does accurately target a given question within the questions array
-  questions.answers.forEach((answer) => {                        
+//showQuestion first manipulates the text of questionEl variable by targeting the array parameter passed to the function
+//and appending the value of question onto it from the object array(questions => question => answers => option). We then
+//target the answers property of the object array, putting it through a forEach loop. The forEach loop creates a button
+//variable- which creates a button element- for each answer in array.answers. It targets the text of each button with
+//answer.option, populating each button element with each avaliable option property, and appends the button variable onto
+//the answerButtonsEl variable, creating a button for each of the given answer.option values. A class of 'btn' is added to
+//each button variable appended. Finally, an event listener is attached to the button variable, activating on a click and
+//calling the selectAnswer function.
+function showQuestion(array) {
+  questionEl.innerText = array.question;
+  array.answers.forEach((answer) => {                        
     let button = document.createElement("button");
     button.innerText = answer.option;
     answerButtonsEl.appendChild(button);
     button.classList.add("btn");
     // if (answer.correct) {
     //   button.dataset.correct = answer.correct;
-    // }   I believe this had previously determined if a selected answer was correct or not, not sure if needed                          
+    // }   I believe this had previously determined if a selected answer was correct or not, not sure if needed here                         
     button.addEventListener("click", selectAnswer);
   });
 };
 
+//selectAnswer checks if the length of the nextQuestion array(which is 5) IS GREATER THAN the number which currentQuestion
+//evaluates to + 1. If nextQuestion.length is greater, then it will remove the class of 'hide' from the nextButton element,
+//making it visible. If nextQuestion.length is NOT GREATER than currentQuestion + 1(which will happen once the user has
+//reached the end of our array of questions), it will instead remove the class of 'hide' from the scoreButton variable and
+//determine its text, making the Highscore button visible. It will also add the class of 'hide' to an element with an Id of
+//'time-readout' which makes the timer not visible.
 function selectAnswer() {
   if (nextQuestion.length > currentQuestion + 1) {
     nextButton.classList.remove("hide");
   } else {
-    scoreButton.innerHTML = "Highscores";
     scoreButton.classList.remove("hide");
-    document.getElementById(`time-readout`).classList.add(`hide`); //removes the timer display on displaying the highscore button
+    scoreButton.innerHTML = "Highscores";
+    document.getElementById(`time-readout`).classList.add(`hide`);
   }
 };
 
+//The timer function below is a very good example of a basic countdown. Within this app, it is called in the startGame function,
+//ensuring the user is immediatly timed upon beginning the quiz. First, we make a variable of timeCount which is used to call
+//setInterval. The first argument of setInterval is a nameless function; this function selects an element with an Id of
+//'time-readout' and gives it the text of 'Time left:' with a template literal of ${sec}, which allows the countdown to
+//dynamically update as time passes. We then enter an if statement that checks if sec IS GREATER THAN 0(checking if we've
+//ran out of time) and decrements sec if it is greater than zero. If sec is NOT GREATER than 0, we call clearInterval with an
+//argument of timeCount- clearInterval being a built-in tool of setInterval which will interrupt its typical process(in this
+//case, decrementing sec). We also have a secondary if statement that checks if currentQuestion evaluates to the same number
+//as questions.length - 1(which would mean the user has reached the end of our quiz), and if so also calls clearInterval with
+//an argument of timeCount. Finally, we have setIntervals second argument- after the curly brace with a comma- 1000, which is
+//a measurment of milliseconds we'd like the setInterval function to delay itself by(evaluating to a 1 second passage of time).
 function timer() {
   const timeCount = setInterval(function () {
     document.getElementById("time-readout").innerHTML = `Time left: ${sec}`;
@@ -176,7 +198,7 @@ startButton.addEventListener("click", startGame);
 
 //below is a possible quick-ref on how to impliment score-saving via methods on a different repo
 // let saveTasks = function() {
-//   localStorage.setItem("tasks", JSON.stringify(tasks));//stringify converts an array into a number
+//   localStorage.setItem("tasks", JSON.stringify(tasks));
 // }
 // let loadTasks = function() {
 //   let savedTasks = localStorage.getItem("tasks"); //this variable here allows us to save added tasks between sessions
